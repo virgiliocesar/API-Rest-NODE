8328b332ec64d10055e094a38559f5d68aac8070
virgílio conceição césar <virgilio_cesar.dev@outlook.com>
2024-06-24 22:43:51 -0300

    criado arquivo mocado teste de rota get

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const port = 3000;
+import app from "./src/app.js"
+
+const PORT = 3000;
 
 //* listen to the door 3000
-app.listen(port, () => {
-    console.log(`server running at address http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`server running at address http://localhost:${PORT}`);
 })
--- src/app.js
+++ src/app.js
@@ -1,9 +1,41 @@
-const express = require('express')
-const app = express()
+import express from "express";
 
+const app = express();
+
+//* mock
+const selections = [
+  {
+    id: 1,
+    selection: "Brasil",
+    group: "G",
+  },
+  {
+    id: 2,
+    selection: "Portugal",
+    group: "G",
+  },
+  {
+    id: 3,
+    selection: "Espanha",
+    group: "G",
+  },
+  {
+    id: 4,
+    selection: "Italia",
+    group: "G",
+  },
+];
 
 //* default route or root route
-app.get('/', (req, res) => {
-    res.send('default route')
-})
+app.get("/", (req, res) => {
+  res.send("default route ");
+});
 
+//* section route
+app.get("/selections", (req, res) => {
+  res.status(200).send(
+    selections
+  );
+});
+
+export default app;
